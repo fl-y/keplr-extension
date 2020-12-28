402ea7bb55c5b8d87148bf74a282c5d2d46b922e
Thunnini <yunjh1994@gmail.com>
2020-12-28 22:42:38 +0900

    Add initial `router` package

---
+++ packages/router/src/sender/extension.ts
@@ -0,0 +1,41 @@
+import { MessageRequester } from "../types";
+import { Message } from "../message";
+
+export class InExtensionMessageRequester implements MessageRequester {
+  async sendMessage<M extends Message<unknown>>(
+    port: string,
+    msg: M
+  ): Promise<M extends Message<infer R> ? R : never> {
+    msg.validateBasic();
+
+    // Set message's origin.
+    // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+    // @ts-ignore
+    msg["origin"] = window.location.origin;
+
+    return await browser.runtime.sendMessage({
+      port,
+      type: msg.type(),
+      msg
+    });
+  }
+
+  static async sendMessageToTab<M extends Message<unknown>>(
+    tabId: number,
+    port: string,
+    msg: M
+  ): Promise<M extends Message<infer R> ? R : never> {
+    msg.validateBasic();
+
+    // Set message's origin.
+    // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+    // @ts-ignore
+    msg["origin"] = window.location.origin;
+
+    return await browser.tabs.sendMessage(tabId, {
+      port,
+      type: msg.type(),
+      msg
+    });
+  }
+}
