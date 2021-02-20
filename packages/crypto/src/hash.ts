8eca354738a45ed373db24776d425fa056e32752
Thunnini <yunjh1994@gmail.com>
2021-02-21 00:42:16 +0900

    Minor change for the hash function

--- packages/crypto/src/hash.ts
+++ packages/crypto/src/hash.ts
@@ -1,12 +1,8 @@
 import { sha256 } from "sha.js";
 
-import { Buffer } from "buffer/";
-
 export class Hash {
   static sha256(data: Uint8Array): Uint8Array {
-    return new Uint8Array(
-      Buffer.from(new sha256().update(data).digest("hex"), "hex")
-    );
+    return new Uint8Array(new sha256().update(data).digest());
   }
 
   static truncHashPortion(
